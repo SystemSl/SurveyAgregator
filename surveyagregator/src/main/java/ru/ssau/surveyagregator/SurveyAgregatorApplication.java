6f48b9fbfe0469db2c3ff60f59175735910324cc
SystemSl <Dmitriy.sulimov2003@gmail.com>
2025-03-14 13:10:28 +0400

    Spring project initialization

---
+++ surveyagregator/src/main/java/ru/ssau/surveyagregator/SurveyAgregatorApplication.java
@@ -0,0 +1,13 @@
+package ru.ssau.surveyagregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyAgregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveyAgregatorApplication.class, args);
+	}
+
+}

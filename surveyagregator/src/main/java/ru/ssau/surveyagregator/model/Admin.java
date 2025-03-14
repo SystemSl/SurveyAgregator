c841ab236065799adaca1437ec8a1a5141058c9a
FingerOnTheEjectButton <145351625+FingerOnTheEjectButton@users.noreply.github.com>
2025-03-14 13:49:24 +0400

    Creating admin model

---
+++ surveyagregator/src/main/java/ru/ssau/surveyagregator/model/Admin.java
@@ -0,0 +1,70 @@
+package ru.ssau.surveyagregator.model;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "admins")
+public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer adminid;
+
+    private String name;
+    private String email;
+    private String password;
+
+    public Admin() {
+    }
+
+    public Admin(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Integer getAdminId() {
+        return adminid;
+    }
+
+    public void setAdminId(Integer adminid) {
+        this.adminid = adminid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = {
+            CascadeType.PERSIST,
+            CascadeType.MERGE
+    })
+    @JoinTable(
+            name = "surveycreators",
+            joinColumns = { @JoinColumn(name = "adminid") },
+            inverseJoinColumns = { @JoinColumn(name = "surveyid") })
+    private Set<Survey> surveys = new HashSet<>();
+
+}

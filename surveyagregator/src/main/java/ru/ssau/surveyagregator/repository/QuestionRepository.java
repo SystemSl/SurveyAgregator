4759ba98d66824b03a5ffc931a3d00842d6c02b1
SystemSL <Dmitriy.sulimov2003@gmail.com>
2025-04-06 14:44:09 +0400

    Update

--- surveyagregator/src/main/java/ru/ssau/surveyagregator/repository/QuestionRepository.java
+++ surveyagregator/src/main/java/ru/ssau/surveyagregator/repository/QuestionRepository.java
@@ -1,10 +1,12 @@
 package ru.ssau.surveyagregator.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.ssau.surveyagregator.model.Question;
 
 import java.util.UUID;
 
+@Repository
 public interface QuestionRepository extends JpaRepository<Question, UUID> {
 
 }
--- surveyagregator/src/main/java/ru/ssau/surveyagregator/repository/SurveyRepository.java
+++ surveyagregator/src/main/java/ru/ssau/surveyagregator/repository/SurveyRepository.java
@@ -3,11 +3,13 @@ package ru.ssau.surveyagregator.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import ru.ssau.surveyagregator.model.Survey;
 
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface SurveyRepository extends JpaRepository<Survey, UUID> {
     @Query("SELECT s FROM Survey s JOIN s.users u WHERE u.id = :userId")
     List<Survey> findSurveysByAdminId(@Param("userId") UUID userId);

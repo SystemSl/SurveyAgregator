4c34db6c444491e52d8e14a9b5bd3960afb3bd6a
FingerOnTheEjectButton <145351625+FingerOnTheEjectButton@users.noreply.github.com>
2025-05-16 20:21:37 +0400

    Readme update & containerization (#3)
    * Refactoring
    
    * Docker + refactor
    
    * swagger ui
    
    * readme.md update
    
    * readme update v2
    
    * jpg fix

--- service/src/main/java/ru/ssau/surveyagregator/client/ClientBeans.java
+++ service/src/main/java/ru/ssau/surveyagregator/client/ClientBeans.java
@@ -1,15 +1,19 @@
 package ru.ssau.surveyagregator.client;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 public class ClientBeans {
     @Bean
-    public SurveyAgregatorRestClientImpl surveyAgregatorRestClient() {
+    public SurveyAgregatorRestClientImpl surveyAgregatorRestClient(
+            @Value("${surveyagregator.api}") String apiUri
+    ) {
         return new SurveyAgregatorRestClientImpl(RestClient.builder()
-                .baseUrl("http://localhost:8080")
+                .baseUrl(apiUri)
                 .build());
     }
 }
